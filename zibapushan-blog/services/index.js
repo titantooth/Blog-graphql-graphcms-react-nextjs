f3489a31f94336ccc35c8b1b6cbfc1aa60311067
titantooth <ali.rabieeie@gmail.com>
2023-08-29 19:09:52 +0330

    wrote query

--- zibapushan-blog/services/index.js
+++ zibapushan-blog/services/index.js
@@ -2,9 +2,33 @@ import { request, gql } from "graphql-request";
 
 export const getPosts = async () => {
   const query = gql`
-        query MyQuery {
-            
+    query MyQuery {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
         }
-    
-    `;
+      }
+    }
+  `;
 };
