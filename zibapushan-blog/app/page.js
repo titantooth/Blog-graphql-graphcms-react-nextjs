f8a05657e1ac21eb91f4244d0686faede0ba2ef8
titantooth <ali.rabieeie@gmail.com>
2023-08-30 00:18:46 +0330

    dont get it T-T

--- zibapushan-blog/app/page.js
+++ zibapushan-blog/app/page.js
@@ -1,26 +1,18 @@
-import Image from "next/image";
+import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-import { PostCard, Categories, PostWidget, Header } from "../components";
-
-const posts = [
+export default function Home({ posts }) {
   {
-    title: "React Testing",
-    excerpt: "Learn React Testing",
-  },
-  {
-    title: "React with Tailwind",
-    excerpt: "Learn React with Tailwind",
-  },
-];
-
-export default function Home() {
+    console.log(posts);
+  }
   return (
-    <div className="container mx-auto px-10 mb-8 ">
+    <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
-          ))}
+          {console.log(posts)}
+          {/* {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))} */}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -31,4 +23,11 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
--- zibapushan-blog/services/index.js
+++ zibapushan-blog/services/index.js
@@ -1,20 +1,13 @@
 import { request, gql } from "graphql-request";
 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
       postsConnection {
         edges {
-          cursor
           node {
-            author {
-              bio
-              name
-              id
-              photo {
-                url
-              }
-            }
             createdAt
             slug
             title
@@ -26,9 +19,21 @@ export const getPosts = async () => {
               name
               slug
             }
+            author {
+              bio
+              id
+              name
+              photo {
+                url
+              }
+            }
           }
         }
       }
     }
   `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
 };
