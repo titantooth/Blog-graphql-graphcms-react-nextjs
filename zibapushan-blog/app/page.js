e00c6d6d97910c06d2368cbc4111814a3c541fb5
titantooth <ali.rabieeie@gmail.com>
2023-08-29 19:03:39 +0330

    added layout header to whole app added api with gql and cms

--- zibapushan-blog/app/page.js
+++ zibapushan-blog/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 import { PostCard, Categories, PostWidget, Header } from "../components";
 
 const posts = [
@@ -15,7 +16,6 @@ const posts = [
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8 ">
-      <Header />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
---
+++ zibapushan-blog/services/index.js
@@ -0,0 +1,10 @@
+import { request, gql } from "graphql-request";
+
+export const getPosts = async () => {
+  const query = gql`
+        query MyQuery {
+            
+        }
+    
+    `;
+};
