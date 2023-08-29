cee06ec7c79ac664b298fb9b1393da92d8b2a5eb
titantooth <ali.rabieeie@gmail.com>
2023-08-29 11:50:16 +0330

    added globals.scss file and styled a bit then imported to layout,js

--- zibapushan-blog/app/page.js
+++ zibapushan-blog/app/page.js
@@ -14,7 +14,7 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8 ">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
