5b612441c82028f7c63c95b3d85e1ad73268ed95
titantooth <ali.rabieeie@gmail.com>
2023-08-30 09:39:40 +0330

    the api returns correct

--- zibapushan-blog/app/page.js
+++ zibapushan-blog/app/page.js
@@ -1,19 +1,13 @@
+// import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
-  {
-    console.log(posts);
-  }
   return (
     <div className="container mx-auto px-10 mb-8">
+      {/* <FeaturedPosts /> */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {console.log(posts)}
-          {/* {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))} */}
-        </div>
+        <div className="lg:col-span-8 col-span-1">{posts.node}</div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
@@ -25,9 +19,10 @@ export default function Home({ posts }) {
   );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
-    props: { posts },
+    props: posts,
   };
 }
